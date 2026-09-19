33b0074bb0f2fec8a6308e80940bc2fa4c4c5b60
ntua-el21637 <el21637@mail.ntua.gr>
2026-09-19 23:39:00 +0300

    Create q96.sql

---
+++ plan2/level1/q96.sql
@@ -0,0 +1,15 @@
+SELECT "count"(*)
+FROM
+  cassandra.db_cassandra.store_sales
+, mariadb.db_infsystems.household_demographics
+, mariadb.db_infsystems.time_dim
+, ignite."public".store
+WHERE ("ss_sold_time_sk" = "time_dim"."t_time_sk")
+   AND ("ss_hdemo_sk" = "household_demographics"."hd_demo_sk")
+   AND ("ss_store_sk" = "s_store_sk")
+   AND ("time_dim"."t_hour" = 20)
+   AND ("time_dim"."t_minute" >= 30)
+   AND ("household_demographics"."hd_dep_count" = 7)
+   AND ("store"."s_store_name" = 'ese')
+ORDER BY "count"(*) ASC
+LIMIT 100;
